e7aa1df2aaeae30a84c0ac20409187c24c0982f7
tetuyosi <tetuyosi39@gmail.com>
2017-04-18 20:49:37 +0900

    remove break

--- lib/fluent/plugin/in_mysql_appender_multi.rb
+++ lib/fluent/plugin/in_mysql_appender_multi.rb
@@ -77,18 +77,17 @@ module Fluent
           start_time = Time.now
           db = get_connection
           db.query(get_query(config, last_id)).each do |row|
-            if rows_count > 0 && (last_id.to_i + 1) != row[config['primary_key']] then
-              break
+            if rows_count == 0 || (last_id.to_i + 1) == row[config['primary_key']] then
+              if config['time_column'].nil? then
+                  td_time = Engine.now
+              else
+                td_time = get_time(row[config['time_column']]).to_i
+              end
+              row.each {|k, v| row[k] = v.to_s if v.is_a?(Time) || v.is_a?(Date) || v.is_a?(BigDecimal)}
+              router.emit(tag, td_time, row)
+              rows_count += 1
+              last_id = row[config['primary_key']]
             end
-            if config['time_column'].nil? then
-                td_time = Engine.now
-            else
-              td_time = get_time(row[config['time_column']]).to_i
-            end
-            row.each {|k, v| row[k] = v.to_s if v.is_a?(Time) || v.is_a?(Date) || v.is_a?(BigDecimal)}
-            router.emit(tag, td_time, row)
-            rows_count += 1
-            last_id = row[config['primary_key']]
           end
           db.close
           elapsed_time = sprintf("%0.02f", Time.now - start_time)
